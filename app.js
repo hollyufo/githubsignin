43049344f2106beef75387321c1e140cd87ac449
imranchaibi <imranchaibi1@gmail.com>
2021-01-16 13:56:41 +0100

    test

---
+++ app.js
@@ -0,0 +1,67 @@
+       
+        function search(){
+            var username= document.getElementById("username").value;
+
+            console.log("https://api.github.com/users/"+username);
+
+  
+
+            var table = new XMLHttpRequest;
+            var DATA = "https://api.github.com/users/"+username;
+
+            table.open("GET",DATA,true);
+            var objSerch;
+             table.onload = function(){
+             if (this.status == 200){
+
+                 jsonTable = JSON.parse(this.responseText);
+                objSerch = {
+                login:jsonTable.login,
+                avatar:jsonTable.avatar_url,
+                repos:jsonTable.public_repos,
+               followers:jsonTable.followers
+             }
+
+             document.getElementById("imgProfil").src= objSerch.avatar;	
+             document.getElementById("nameProfil").innerHTML= objSerch.login;	
+             document.getElementById("followers").innerHTML= objSerch.followers	;
+             document.getElementById("repos").innerHTML= objSerch.repos	;	
+             document.getElementById("link").target= "_blank";
+             document.getElementById("link").href= "https://github.com/"+objSerch.login	
+             ;  
+           } else{
+                document.getElementById("imgProfil").src= "404.jpg";	
+             document.getElementById("nameProfil").innerHTML= "Profile not found ! ";	
+             document.getElementById("followers").innerHTML= "00";
+             document.getElementById("repos").innerHTML= "00";
+              document.getElementById("link").href= "#";
+              document.getElementById("link").target= "";
+
+             }  
+            
+           }
+            table.send();
+        }
+
+        function load() {
+        	   var arrr=[];
+       		   var obj ;
+   fetch('https://api.github.com/users')
+                 .then(res => res.json())//response type 
+                 .then(data => { 
+
+                        for(var i =0;i<4;i++){                         
+                  obj={
+                      login:data[i].login,
+                      avatar:data[i].avatar_url
+                        } 
+                  arrr.push(obj);
+                  var name = "name"+i;
+                  var image= "src"+i
+                  document.getElementById(name).innerHTML=arrr[i].login ;  
+                  document.getElementById(image).src=arrr[i].avatar ;  
+               
+                     }
+
+                 }); 
+        }
---
+++ login.js
@@ -0,0 +1,42 @@
+
+function conpass() {
+
+        // entered data from the login-form
+        var userid = document.getElementById('userid');
+        var userPw = document.getElementById('pass');
+        var userconPw = document.getElementById('compass');
+        //here to check if the password match
+        if(userPw.value != userconPw.value) {
+            alert("your password dont match")
+    }
+        else if (userPw.value = userconPw.value) {
+            console.log("right")
+            localStorage.setItem("n1", userid.value);
+            localStorage.setItem("p1", userPw.value);
+            setTimeout(function(){
+                window.location.href = 'login.html';
+            }, 700);
+    }
+}
+
+function check() {
+
+    // stored data from the register-form
+    var storedN1 = localStorage.getItem('n1');
+    var storedP1 = localStorage.getItem('p1');
+
+    // entered data from the login-form
+    var userName = document.getElementById('userid');
+    var userPw = document.getElementById('pass');
+
+    // check if stored data from register-form is equal to data from login form
+    if(userName.value !== storedN1 || userPw.value !== storedP1) {
+        alert('ERROR');
+    }else {
+        setTimeout(function(){
+            window.location.href = 'lastpage.html';
+        }, 700);
+    }
+}
+    
+    
